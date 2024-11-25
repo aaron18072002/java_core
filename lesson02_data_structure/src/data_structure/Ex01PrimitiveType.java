5df0c861c09af04006ca42459abda84d08b9972d
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-25 10:24:39 +0700

    updated

---
+++ lesson02_data_structure/src/data_structure/Ex01PrimitiveType.java
@@ -0,0 +1,16 @@
+package data_structure;
+
+public class Ex01PrimitiveType {
+
+	public static void main(String[] args) {
+		int digit; // tao 1 o nho trong vung nho stack
+		digit = 5; // gan gia tri cho o nho do
+		
+		System.out.printf("value -> %d", digit).println();
+		
+		char letter = 'A';
+		float point = 5.5f;
+		double score = 7.77d;
+	}
+
+}
---
+++ lesson02_data_structure/src/data_structure/Ex02PrimitiveReassignValues.java
@@ -0,0 +1,9 @@
+package data_structure;
+
+public class Ex02PrimitiveReassignValues {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
