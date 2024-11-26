823f93dd1643e241ebbbe4f33cfaa9f6000aee9d
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-26 20:21:31 +0700

    updated

--- lesson02_data_structure/src/datastructure/object/Ex06ObjectTypeAvailableDiff.java
+++ lesson02_data_structure/src/datastructure/object/Ex06ObjectTypeAvailableDiff.java
@@ -2,6 +2,16 @@ package datastructure.object;
 
 public class Ex06ObjectTypeAvailableDiff {
 	public static void main(String[] args) {
+		// Giống nhau: KDL đối tượng lưu ở HEAP
 		
+		// Toán tử new
+		// Lưu trữ: HEAP
+		// sử dụng new thì luôn luôn tạo ra 1 ô nhớ mới trong HEAP
+		String s1 = new String("s1");
+		String s2 = new String("s2");
+		String s3 = new String("s3");
+		
+		// Gán trực tiếp 
+		// Lưu trữ: HEAP - CONSTANT pool
 	}
 }
