20a829a2fb7549d456693af3beb824b191869daa
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-28 17:55:45 +0700

    updated

---
+++ lesson02_data_structure/src/view/Ex02Operator.java
@@ -0,0 +1,9 @@
+package view;
+
+public class Ex02Operator {
+	public static void main(String[] args) {
+		// Toán tử kết hợp && || --> toán tử luận lý
+		// &&: BT trả về TRUE nếu tất cả BT đều TRUE
+		//	 : Nếu 1 biểu thức là FALSE thì dừng lại không kiểm tra các BT sau 
+	}
+}
---
+++ lesson02_data_structure/src/view/Ex03IOScanner.java
@@ -0,0 +1,7 @@
+package view;
+
+public class Ex03IOScanner {
+	public static void main(String[] args) {
+		
+	}
+}
