a609077c3b6cfb477e3c25aa2c6fc5f6bc0538f5
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-04 05:04:42 +0700

    updated

--- lesson03_oop/src/views/Ex04StaticMethodDemo.java
+++ lesson03_oop/src/views/Ex04StaticMethodDemo.java
@@ -1,11 +1,45 @@
 package views;
 
 public class Ex04StaticMethodDemo {
-	public static void main(String[] args) {
-		
+	private final int n;
+	
+	public Ex04StaticMethodDemo(int n) {
+		this.n = n;
 	}
 	
+	public static void main(String[] args) {
+		int x1 = 12;
+		int x2 = 8;
+
+		System.out.println("Sum from o1 --> " + Ex04StaticMethodDemo.sum(x1, x2));
+		
+		Ex04StaticMethodDemo o1 = new Ex04StaticMethodDemo(5);		
+		System.out.println("n! = " + o1.factorial());
+		
+		Ex04StaticMethodDemo o2 = new Ex04StaticMethodDemo(6);		
+		System.out.println("n! = " + o2.factorial());
+		
+		Ex04StaticMethodDemo o3 = new Ex04StaticMethodDemo(7);		
+		System.out.println("n! = " + o3.factorial());
+	}
+	
+	// Hàm chỉ phụ thuộc vào
+	// tham số truyền vào a,b, ko phụ thuộc vào đối tượng đang gọi
+	// --> nên là static method
 	private static int sum(int a, int b) {
 		return a + b;
+	}
+	
+	// Hàm nên để non-static
+	// getter, setter, toString
+	// Viết hàm tính n! yêu cầu non-static
+	public long factorial() {
+		long sum = this.n;
+		int number = this.n;
+		while(number > 1) {
+			number -= 1;
+			sum *= number;
+		}
+		return sum;
 	}
 }
