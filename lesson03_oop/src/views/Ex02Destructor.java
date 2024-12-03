cb08f278c110153a74c18a93cd82f74e7742e8f7
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-04 00:50:25 +0700

    updated

---
+++ lesson03_oop/src/views/Ex02Destructor.java
@@ -0,0 +1,24 @@
+package views;
+
+import beans.Car;
+
+public class Ex02Destructor {
+	public static void main(String[] args) {
+		Car c1 = new Car(1,"A1","A11 name",220d); // H1
+		
+		Car c2 = new Car(2,"A2","A22 name",280d); // H2
+		
+		Car c3 = c1;
+		
+		c2 = new Car(22,"A22","A22 name", 440d); // H4
+		
+		// Ô nhớ, đối tượng ở vị trí H2 không còn được liên kết bởi biến nào
+		// ở STACK --> unused, với Java sẽ tự clear những unused objects
+		// thông qua cơ chế GARBAGE COLLECTION
+		
+		// Nguyên nhân
+		// Biến trỏ đến đối tượng đó, nó trỏ đến đối tượng hay ô nhớ khác
+		
+		// Có cách nào hạn chế tạo ra các unused objects không?
+	}
+}
