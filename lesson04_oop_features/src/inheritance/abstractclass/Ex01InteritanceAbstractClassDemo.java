0ef314301c21a94b8a70cfd27a7f9e5cbdf30447
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-05 14:31:26 +0700

    updated

--- lesson04_oop_features/src/inheritance/abstractclass/Ex01InteritanceAbstractClassDemo.java
+++ lesson04_oop_features/src/inheritance/abstractclass/Ex01InteritanceAbstractClassDemo.java
@@ -1,7 +1,19 @@
 package inheritance.abstractclass;
 
 public class Ex01InteritanceAbstractClassDemo {
+	
 	public static void main(String[] args) {
+		EmailService em1 = new StandardEmailService();
 		
+		// anonymous class
+		EmailService em2 = new EmailService() {		
+			@Override
+			void login() {
+				System.out.println("Anonymous em2 # login");
+			}
+		};
+		em2.login();
+		em2.login2Steps();
 	}
+	
 }
