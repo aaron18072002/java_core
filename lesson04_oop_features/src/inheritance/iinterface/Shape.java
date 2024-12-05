50da560c9acdd28ffed0e42726cf674e0a09dfd4
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-05 13:42:02 +0700

    updated

---
+++ lesson04_oop_features/src/inheritance/iinterface/Shape.java
@@ -0,0 +1,10 @@
+package inheritance.iinterface;
+
+public interface Shape {
+	// 1 hàm trong interface mặc định có từ khóa 
+	// là abstract và public
+	// hàm bên dưới chỉ có phần khai báo, ko có thân hàm
+	// --> hàm trừu tượng, abstract method
+	
+	void calcS();
+}
