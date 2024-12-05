59bb275867fcaf0d195a62f8db7f0afc6a5fe1cc
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-05 13:50:22 +0700

    updated

---
+++ lesson04_oop_features/src/inheritance/iinterface/Ex01InheritanceInterfaceDemo.java
@@ -0,0 +1,15 @@
+package inheritance.iinterface;
+
+public class Ex01InheritanceInterfaceDemo {
+	public static void main(String[] args) {
+		// anonymous inner type
+		// anonymous class --> lớp ẩn danh
+		Shape shape1 = new Shape() {			
+			@Override
+			public void calcS() {
+				System.out.println("Circle#calcS ...implementation");
+			}
+		};
+		shape1.calcS();
+	}
+}
