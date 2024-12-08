effc2d5c5a816b8a1509c59a507659dba6d8b20b
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-08 19:35:51 +0700

    updated

--- lesson07_libraries/src/view/datetime/Ex01BasicDatetimeDemo.java
+++ lesson07_libraries/src/view/datetime/Ex01BasicDatetimeDemo.java
@@ -1,8 +1,22 @@
 package view.datetime;
 
+import java.util.Date;
+
 public class Ex01BasicDatetimeDemo {
 
 	public static void main(String[] args) {
+		
+		// Lưu trữ Date, Calendar
+		
+		// Múi giờ 
+		// ICT: Indochina time: giờ đông dương
+		
+		// Mặc định: lấy theo múi giờ hiện tại của máy đang triển khai
+		// Có thể set múi giờ nào
+		
+		// Lấy ra time hiện tại
+		Date date = new Date();
+		System.out.println("1. Date --> " + date);
 		
 	}
 	
