2bebc02491d1edb38417dfbf1c120a98d959f611
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-08 14:07:32 +0700

    updated

--- lesson06_exception/src/view/Ex06CustomException.java
+++ lesson06_exception/src/view/Ex06CustomException.java
@@ -1,9 +1,20 @@
 package view;
 
+import exception.InvalidAgeException;
+
 public class Ex06CustomException {
 	
 	public static void main(String[] args) {
+		System.out.println("validate 16 --> " + validate(16));
 		
+		System.out.println("validate 12 --> " + validate(12));
+	}
+	
+	private static boolean validate(int age) {
+		if(age < 16) {
+			throw new InvalidAgeException();
+		}
+		return true;
 	}
 	
 }
