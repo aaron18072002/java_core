3fedb080f4338860ae41569f3f6929274ddd0c57
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-09 16:34:48 +0700

    updated

--- lesson07_libraries/src/view/datetime/Ex04Exercises.java
+++ lesson07_libraries/src/view/datetime/Ex04Exercises.java
@@ -2,6 +2,7 @@ package view.datetime;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
@@ -28,18 +29,65 @@ public class Ex04Exercises {
 		// 3. In thông tin các ngày trong tháng, tuần hiện tại
 		// dd/MM/yyyy week_day
 		printDaysInMonth(c);
+		printDaysInWeek(c);
 		
 		// 4. Đếm xem trong tháng có bao nhiêu ngày chủ nhật và in ra
+		c.set(Calendar.MONTH, Calendar.OCTOBER);
+		Calendar[] sundays = getSundays(c);
+		System.out.println("Số ngày chủ nhật --> " + sundays.length);
+		df = new SimpleDateFormat("dd/MM/yyyy EEEE");
+		for(Calendar sunday:sundays) {
+			System.out.println(df.format(sunday.getTime()));
+		}
+	}
+	
+	private static Calendar[] getSundays(Calendar source) {
+		Calendar start = cloneAndSet(source, Calendar.DAY_OF_MONTH, 1);
+		Calendar end = cloneAndSet(source, Calendar.DAY_OF_MONTH, 
+				source.getActualMaximum(Calendar.DAY_OF_MONTH));
+		end.add(Calendar.DAY_OF_MONTH, 1);
+		
+		Calendar[] target = new Calendar[10];
+		int count = 0;
+		
+		for(Calendar c = start; c.before(end);) {
+			if(c.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+				Calendar clone = clone(c);
+				target[count++] = clone;
+				c.add(Calendar.DAY_OF_MONTH, 7);
+				continue;
+			}
+			c.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		
+		return Arrays.copyOfRange(target, 0, count);
+	}
+	
+	private static void printDaysInWeek(Calendar source) {
+		Calendar start = clone(source);
+		start.add(Calendar.DAY_OF_MONTH, start.getFirstDayOfWeek() - 
+				start.get(Calendar.DAY_OF_MONTH));
+		Calendar end = clone(source);
+		end.set(Calendar.DAY_OF_MONTH, start.get(Calendar.DAY_OF_MONTH) + 7);
+		
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy EEEE");
+		
+		System.out.println("\nCác ngày trong tuần hiện tại --> {");
+		for(Calendar c = start; c.before(end); c.add(Calendar.DAY_OF_MONTH, 1)) {
+			System.out.println("\t" + df.format(c.getTime()));
+		}
+		System.out.println("}");
 	}
 	
 	private static void printDaysInMonth(Calendar source) {
 		Calendar start = cloneAndSet(source, Calendar.DAY_OF_MONTH, 1);
 		Calendar end = cloneAndSet(source, Calendar.DAY_OF_MONTH, 
-				source.getActualMaximum(Calendar.MONTH));
+				source.getActualMaximum(Calendar.DAY_OF_MONTH));
+		end.add(Calendar.DAY_OF_MONTH, 1);
 		
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy EEEE");
 		
-		System.out.println("\nCác ngày tháng hiện tại --> {");
+		System.out.println("\nCác ngày trong tháng hiện tại --> {");
 		for(Calendar c = start; c.before(end); c.add(Calendar.DAY_OF_MONTH, 1)) {
 			System.out.println("\t" + df.format(c.getTime()));
 		}
@@ -50,6 +98,12 @@ public class Ex04Exercises {
 		Calendar target = Calendar.getInstance();
 		target.setTimeInMillis(source.getTimeInMillis());
 		target.set(field, value);
+		return target;
+	}
+	
+	private static Calendar clone(Calendar source) {
+		Calendar target = Calendar.getInstance();
+		target.setTimeInMillis(source.getTimeInMillis());
 		return target;
 	}
 	
---
+++ lesson07_libraries/src/view/datetime/Ex05Dating.java
@@ -0,0 +1,9 @@
+package view.datetime;
+
+public class Ex05Dating {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
