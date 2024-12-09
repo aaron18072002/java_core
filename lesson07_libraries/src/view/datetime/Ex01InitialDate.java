cc1352799107a1d226854a7deb425a5c6150240f
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-09 09:11:01 +0700

    updated

--- lesson07_libraries/src/view/datetime/Ex01InitialDate.java
+++ lesson07_libraries/src/view/datetime/Ex01InitialDate.java
@@ -2,7 +2,7 @@ package view.datetime;
 
 import java.util.Date;
 
-public class Ex01BasicDatetimeDemo {
+public class Ex01InitialDate {
 
 	public static void main(String[] args) {
 		
@@ -18,7 +18,12 @@ public class Ex01BasicDatetimeDemo {
 		Date date = new Date();
 		System.out.println("1. Date --> " + date);
 		
-		date = new Da
+		date = new Date(2024,10,20);
+		System.out.println("2. Date arbitrary --> " + date);
+		
+		// Máy tính: Mốc thời gian 1.1.1970 6:0:0 UTC+0
+		date = new Date(21600000);
+		System.out.println("3. Date arbitrary --> " + date);
 		
 	}
 	
---
+++ lesson07_libraries/src/view/datetime/Ex02InitialCalendar.java
@@ -0,0 +1,12 @@
+package view.datetime;
+
+public class Ex02InitialCalendar {
+	
+	public static void main(String[] args) {
+		// Calendar: nhiều loại lịch(âm/dương)
+		//
+		
+		
+	}
+	
+}
