045537c8ae05476585f1a1af8149c513c888e81f
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-15 10:51:45 +0700

    updated

--- lesson00_design_pattern/src/singleton/RemoteDate.java
+++ lesson00_design_pattern/src/singleton/RemoteDate.java
@@ -2,6 +2,32 @@ package singleton;
 
 public class RemoteDate {
 	
+	private static RemoteDate INSTANCE = null;
+	
 	private int dayOfYear;
+	
+	private RemoteDate() {
+		
+	}
+	
+	public static RemoteDate getInstance() {
+		if(INSTANCE == null) {
+			INSTANCE = new RemoteDate();
+		}
+		return INSTANCE;
+	}
+
+	public int getDayOfYear() {
+		return dayOfYear;
+	}
+
+	public void setDayOfYear(int dayOfYear) {
+		this.dayOfYear = dayOfYear;
+	}
+
+	@Override
+	public String toString() {
+		return "RemoteDate[dayOfYear=" + dayOfYear + "]";
+	}
 	
 }
