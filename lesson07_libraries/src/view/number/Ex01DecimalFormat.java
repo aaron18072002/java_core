f8bb45f098244ed71ab97342e37ac736eeeff8ce
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-16 15:47:30 +0700

    updated

---
+++ lesson07_libraries/src/view/number/Ex01DecimalFormat.java
@@ -0,0 +1,11 @@
+package view.number;
+
+import java.text.DecimalFormat;
+
+public class Ex01DecimalFormat {
+
+	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("#,###.000");
+	}
+	
+}
---
+++ lesson07_libraries/src/view/number/Ex02BigDecimalDemo.java
@@ -0,0 +1,9 @@
+package view.number;
+
+public class Ex02BigDecimalDemo {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
