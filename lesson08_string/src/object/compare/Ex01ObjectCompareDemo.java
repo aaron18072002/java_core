8c9a0476a207d35507213870e70a3c6f5063ece9
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-23 18:14:39 +0700

    update

--- lesson08_string/src/object/compare/Ex01ObjectCompareDemo.java
+++ lesson08_string/src/object/compare/Ex01ObjectCompareDemo.java
@@ -1,5 +1,20 @@
 package object.compare;
 
 public class Ex01ObjectCompareDemo {
+	/*
+	 	Để so sánh GIÁ TRỊ:
+	 	
+	 	+ KDL nguyên thủy: dùng toán tử > < =
+	 	+ KDL đối tượng:
+	 		--> .equals(o): Hàm ở class 'Object' sau đó sẽ override lại
+	 					  : Mặc định sẽ so sánh với toán tử == (STACK)
+	 		--> .compareTo(o): Hàm ở interface Comparable<T> sau
+	 							đó sẽ override lại
+	 	
+	 */
 
+	public static void main(String[] args) {
+		
+	}
+	
 }
