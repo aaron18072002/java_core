de72c79d3790d0bd0cf514a2485c945ff6dea857
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-02-01 14:52:57 +0700

    updated

---
+++ lesson14_threads/src/thread/core/Ex01.java
@@ -0,0 +1,13 @@
+package thread.core;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Program --> Start ...");
+		
+		System.out.println("Program --> End ...");
+		
+	}
+	
+}
