a0c5a892b5aa38b1f4f8a7b2c22a2503053b35f6
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-02-02 15:08:44 +0700

    updated

---
+++ lesson14_threads/src/thread/core/Ex04SimpleThreadFeature.java
@@ -0,0 +1,67 @@
+package thread.core;
+
+import static utils.ThreadUtils.*;
+
+public class Ex04SimpleThreadFeature {
+
+	public static void main(String[] args) {
+		
+		// single thread
+		/*measure("do3TaskFn123", () -> {
+			fn1();
+			fn2();
+			fn3();
+		});*/
+		
+		long start = System.currentTimeMillis();
+		
+		Thread t1 = new Thread(new Runnable() {		
+			@Override
+			public void run() {
+				fn1();
+			}
+		}, "t1");
+		
+		Thread t2 = new Thread(() -> {
+			fn2();
+		}, "t2");
+		
+		Thread t3 = new Thread(() -> {
+			fn3();
+		}, "t3");
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		// đợi 3 thread t1,t2,t3 hoàn thành thì thread #(main) mới đc thực thi
+		// 3 thread t1,t2,t3 vẫn đc xử lý đồng thời
+		join(t1);
+		join(t2);
+		join(t3);
+		
+		long end = System.currentTimeMillis();
+		System.out.println("\n%^&measure " + "do3TaskFn123" + " took ==> " + (end-start) + "(ms)");
+		
+	}
+	
+	private static void fn1() {
+		doTask(4);
+	}
+	
+	private static void fn2() {
+		doTask(4);
+	}
+	
+	private static void fn3() {
+		doTask(4);
+	}
+	
+	private static void measure(String text, Runnable runnable) {
+		long start = System.currentTimeMillis();
+		runnable.run();
+		long end = System.currentTimeMillis();
+		System.out.println("measure " + text + " took ==> " + (end-start) + "(ms)");
+	}
+	
+}
